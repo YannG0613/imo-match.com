4365981f646101c691864651939c667990f3d34f
YannG0613 <yann_gouedo@yahoo.fr>
2025-09-25 23:03:58 +0200

    Update _init__.py

--- auth/_init__.py
+++ auth/_init__.py
@@ -1,10 +1,1 @@
-# auth/__init__.py
-"""Module d'authentification pour ImoMatch"""
-
-try:
-    from .authentication import *
-except ImportError as e:
-    print(f"Warning: Could not import authentication: {e}")
-
-# Vous pouvez aussi faire des imports plus spécifiques :
-# from .authentication import login_user, logout_user, create_user, verify_password
+from .authentication import auth_manager, login_user, logout_user, is_authenticated, get_current_user
