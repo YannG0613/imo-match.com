3c6bcae975c46cb479aae16f463ee6219bfbc815
YannG0613 <yann_gouedo@yahoo.fr>
2025-09-28 08:42:03 +0200

    Update _init__.py

--- database/_init__.py
+++ database/_init__.py
@@ -1,5 +1,1 @@
-# database/__init__.py
-from .manager import DatabaseManager
-
-# Instance globale pour faciliter l'utilisation
-db_manager = DatabaseManager()
+from .manager import db_manager, get_database, search_properties, get_stats
